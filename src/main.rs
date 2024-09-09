7ec2791e753255b0b9314a9a3d3b3f29c72a1d5b
Ismail Karabulut <i.karabulut93@gmail.com>
2024-09-09 13:25:26 -0500

    add command line args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+
+use chrono::{Local, DateTime};
+use clap::{Arg, Command};
+use clap::builder::PossibleValuesParser;
+
+struct Clock;
+
+impl Clock {
+    fn get() -> DateTime<Local> {
+        Local::now()
+    }
+
+    fn set() -> ! {
+        unimplemented!()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let command = Command::new("clock")
+        .about("Gets and sets the time")
+        .arg(
+            Arg::new("action")
+                .long("action")
+                .value_parser(PossibleValuesParser::new(["get", "set"]))
+                .default_value("get")
+        )
+        .arg(
+            Arg::new("std")
+                .short('s')
+                .long("standard")
+                .value_parser(PossibleValuesParser::new(["rfc2822", "rfc3339", "timestamp"]))
+                .default_value("rfc3339")
+        )
+        .arg(
+            Arg::new("datetime")
+                .help("When <action> is 'set', apply <datetime>. \
+                Otherwise, ignore.")
+        );
+
+    let args = command.get_matches();
 }
